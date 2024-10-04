cf0137990abc17cae7aace5c082ba445ff6f99b8
Joshua Whitley <joshua.whitley@bonsairobotics.ai>
2024-10-03 20:54:59 -0700

    Copyright updates

--- src/core/gpai_geometry/include/gpai_geometry/primitives.hpp
+++ src/core/gpai_geometry/include/gpai_geometry/primitives.hpp
@@ -1,4 +1,4 @@
-// Copyright 2024, Electrified Autonomy, LLC
+// Copyright 2024 The GPAI Contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
